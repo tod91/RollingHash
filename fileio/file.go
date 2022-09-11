ff5de3ec95cf0fef267d5c76b7e1c264a10e2201
tod91 <todorivanov066@gmail.com>
2022-09-11 21:33:20 +0300

    added comments

--- fileio/file.go
+++ fileio/file.go
@@ -2,16 +2,24 @@ package fileio
 
 import (
 	"bufio"
+	"errors"
 	"os"
 )
 
-func Open(input string) (*bufio.Reader, error) {
-	// Open file to split
+func Open(input string, blockSize int64) (*bufio.Reader, error) {
+
+	// Read file
 	file, err := os.Open(input)
 	if err != nil {
 		return nil, err
 	}
 
-	return bufio.NewReader(file), nil
+	fileInfo, _ := file.Stat()
 
+	// calc chunk size
+	if fileInfo.Size()/blockSize < 2 {
+		return nil, errors.New("chunk size less than 2")
+	}
+
+	return bufio.NewReader(file), nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"fileio"
+	"RollingHash/fileio"
+	"crypto/md5"
 	"fmt"
 )
 
 func main() {
-	file, err := fileio.Open("test.txt")
+	const blockSize = 8
+
+	_, err := fileio.Open("test/text.txt", blockSize)
 	if err != nil {
 		fmt.Errorf("ERROR: ", err)
 	}
-	//fmt.Printf()
+
 }
+
+// ako chunka mi e po baluk ot 64 togava direkto savveni kontenta
+//1.  idva mi 1 file - cheta go i mu nasmqtam hashovete po blokove i go slagam v vector
+//2. idva mi vtori file - cheta go i mu nasmqtam hashovete po blokove i go slagam v vector
+//3.(-- Tova ti e rolling hash) smqtash levenstein distance i operacii koito da postignesh minimum
+//4. nakraq pravish diff na mestata kudeto si reshil da rpavish change
+// ako byte-ovete sa ti poveche ot poluvinata golemina na bloka smeni change s remove/add
