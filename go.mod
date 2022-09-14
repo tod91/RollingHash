204de2dbb3944f7513b71ca8e054c867b69179ae
todor.ivanov <todor.ivanov@egt-bg.com>
2022-09-14 13:56:52 +0300

    Adding go.mod and gitignore files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module RollingHash
+
+go 1.18
--- main.go
+++ main.go
@@ -22,15 +22,15 @@ func main() {
 	nBlocks, err := hash.Split(nFileContent)
 
 	for i := 0; i < len(nBlocks); i++ {
-		hash.RabinKarp(nBlocks[i])
 		hash.RabinKarp(oBlocks[i])
+		hash.RabinKarp(nBlocks[i])
 	}
 
 }
 
 // ako chunka mi e po baluk ot 64 togava direkto savveni kontenta
-//1.  idva mi 1 file - cheta go i mu nasmqtam hashovete po blokove i go slagam v vector
-//2. idva mi vtori file - cheta go i mu nasmqtam hashovete po blokove i go slagam v vector
+//1.  idva mi 1 file - cheta go i mu nasmqtam hachetashovete po blokove i go slagam v vector
+//2. idva mi vtori file -  go i mu nasmqtam hashovete po blokove i go slagam v vector
 //3.(-- Tova ti e rolling hash) smqtash levenstein distance i operacii koito da postignesh minimum
 //4. nakraq pravish diff na mestata kudeto si reshil da rpavish change
 // ako byte-ovete sa ti poveche ot poluvinata golemina na bloka smeni change s remove/add
